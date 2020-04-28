7206ed4e9f8fcacdc4570e5fc1493df7a8942282
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2020-04-28 20:03:46 +0200

    Eventos

---
+++ AnimalRanking/Assets/Scripts/BotonDerecha.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BotonDerecha : MonoBehaviour
+{
+    public void BotonPulsado()
+    {
+        Debug.Log("Has pulsado el botón derecho.");
+    }
+}
